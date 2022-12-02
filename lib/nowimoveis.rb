0acc27f94757c04beca2077ed645d87a78397662
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2022-12-02 14:41:52 -0300

    F1API-478 Parser for Website integration

--- lib/nowimoveis.rb
+++ lib/nowimoveis.rb
@@ -4,9 +4,59 @@ require_relative 'nowimoveis/version'
 require 'f1sales_custom/parser'
 require 'f1sales_custom/source'
 require 'f1sales_custom/hooks'
+require 'f1sales_helpers'
 
 module Nowimoveis
   class Error < StandardError; end
+
+  class F1SalesCustom::Email::Source
+    def self.all
+      [
+        {
+          email_id: 'website',
+          name: 'Website'
+        }
+      ]
+    end
+  end
+
+  class F1SalesCustom::Email::Parser
+    def parse
+      email_lead
+    end
+
+    def parsed_email
+      @email.body.colons_to_hash(/(Nome|Telefone|Email|Produto|Date).*?:/, false)
+    end
+
+    def email_lead
+      {
+        source: source_name,
+        customer: customer,
+        product: product
+      }
+    end
+
+    def source_name
+      {
+        name: F1SalesCustom::Email::Source.all[0][:name]
+      }
+    end
+
+    def customer
+      {
+        name: parsed_email['nome'],
+        phone: parsed_email['telefone'],
+        email: parsed_email['email']
+      }
+    end
+
+    def product
+      {
+        name: parsed_email['produto'].split("\n").first.strip
+      }
+    end
+  end
 
   class F1SalesCustom::Hooks::Lead
     def self.switch_source(lead)
